f3b5a1e0b3b9349495d475ad1a9fd622ef1bc04d
Pratush Pandita <pratushpandita07@gmail.com>
2022-08-09 19:03:15 +0100

    removing unused files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Autonomous Parking using Reinforcement Learning
+
+This repo contains the code for the dissertation titled "Autonomous Parking using Reinforcement Learning".
--- park_test.py
+++ park_test.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 from stable_baselines3 import SAC
 
 env = gym.make("parking-v0")
+# vertical run
+env.update_config({"gridSizeX": 6,"goalSpotNumber": 2, "duration": 150})
+
 # diagonal run
 # env.update_config({"gridSizeX": 4,"goalSpotNumber": 2, "diagonalShift": 6,"duration": 150})
-
-# vertical run
-# env.update_config({"gridSizeX": 6,"goalSpotNumber": 2, "duration": 150})
 
 # phased 2 scenarios
 # env.update_config({"gridSizeX": 6,"goalSpotNumber": 2, "diagonalShift": 6, "phasedLearning":True, "duration": 150,})
@@ -18,32 +18,23 @@ env = gym.make("parking-v0")
 # env.update_config({"gridSizeX": 6,"goalSpotNumber": 2, "diagonalShift": 6,"randomLearning":True, "duration": 150})
 
 # parallel run
-env.update_config({"gridSizeX": 6,"goalSpotNumber": 2, "duration": 150, "is_parallel_parking": True})
+# env.update_config({"gridSizeX": 6,"goalSpotNumber": 2, "duration": 150, "is_parallel_parking": True})
 
 # random 3 scenarios
 # env.update_config({"gridSizeX": 6,"goalSpotNumber": 2, "diagonalShift": 6, "is_parallel_parking": True,"randomLearning":True, "duration": 150})
 
-
-# print(env.config)
-
 observation = env.reset()
-# img = env.render(mode="rgb_array")
-# plt.imsave('scenarios/scenario45_start.png', img)
 
-model = SAC.load("sac_random_37500_1658884549", env=env)
+model = SAC.load("sac_random_3envs_180000_1659645070", env=env)
 
 for _ in range(5000):
-    # env.render()
     img = env.render(mode="rgb_array")
-
-    # time.sleep(4)
-    # print(action)
-    # action, _ = model.predict(observation, deterministic=True)
-    # observation, reward, done, info = env.step(action) # take a random action
-    # print(observation)
-    # print(info)
-    # if done:
-    #     break
+    action, _ = model.predict(observation, deterministic=True)
+    observation, reward, done, info = env.step(action) # take a random action
+    print(observation)
+    print(info)
+    if done:
+        break
 
 # for _ in range(20):
 #     obs, done = env.reset(), False
@@ -52,6 +43,6 @@ for _ in range(5000):
 #         action, _ = model.predict(obs, deterministic=True)
 #         obs, _, done, _ = env.step(action)
 
-# plt.imsave('scenarios/scenario3.png', img)
+# plt.imsave('scenarios/scenario345.png', img)
 env.terminate()
 env.close()
